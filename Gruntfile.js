6209db0c212024cc8ceb06a74935d01c60cef0e1
bentedder <ben@bentedder.com>
2013-09-05 16:30:26 -0600

    added grunt, package.json, backbone, underscore, jquery, spservices

---
+++ Gruntfile.js
@@ -0,0 +1,80 @@
+module.exports = function(grunt) {
+  'use strict';
+
+  // Project configuration.
+  grunt.initConfig({
+    pkg: grunt.file.readJSON('package.json'),
+    templates: {
+      main: {
+        header: '\n<!-- {{tplbasename}} -->\n<script id="{{tplbasename}}" type="text/template">\n',
+        footer: '\n</script>\n',
+        src: ['src/templates/*.tpl'],
+        dest: 'dist/templates/system.tpl'
+      }
+    },
+    concat: {
+      options: {
+        banner: '/*!\n <%= pkg.name %> Build version <%= pkg.version %>, <%= grunt.template.today("mm-dd-yyyy") %>\n*/\n',
+        separator: ';'
+      },
+      scheduler: {
+        src: [
+            'src/js/libs/jquery-1.9.0.js',
+            'src/js/libs/underscore.js',
+            'src/js/libs/backbone.js',
+            'src/js/libs/jquery.SPServices-0.7.2.js',
+            'src/js/app.js'
+            ],
+        dest: 'dist/js/<%= pkg.name %>.js'
+      },
+      css: {
+        src: 'src/css/style.css',
+        dest: 'dist/css/screen.css'
+      }
+    },
+    uglify: {
+      options: {
+        banner: '/*!\n <%= pkg.name %> Build version <%= pkg.version %>, <%= grunt.template.today("mm-dd-yyyy") %>\n*/\n',
+        report: 'gzip'
+      },
+      dist: {
+        files: {
+          'dist/js/<%= pkg.name %>.min.js': ['<%= concat.scheduler.dest %>']
+        }
+      }
+    },
+    cssmin: {
+      css:{
+        src: '<%= concat.css.dest %>',
+        dest: 'dist/css/screen.min.css'
+      }
+    }
+  });
+
+  grunt.loadNpmTasks('grunt-contrib-concat');
+  grunt.loadNpmTasks('grunt-contrib-uglify');
+  grunt.loadNpmTasks('grunt-contrib-cssmin');
+  
+  
+  // Template mutlitask
+  grunt.registerMultiTask('templates', 'Wraps templates with header and footer, then concats into single file', function () {
+        var data = this.data,
+            path = require('path'),
+            dest = grunt.template.process(data.dest),
+            files = grunt.file.expand(data.src),
+            header = data.header,
+            footer = data.footer,
+            contents = '';
+
+        files.forEach(function(f) {
+            contents += header.replace(/\{\{tplbasename\}\}/g, path.basename(f).replace('.tpl','')) + grunt.file.read(f) + footer;
+        });
+        
+        grunt.file.write(dest, contents);
+        grunt.log.writeln('Template created');
+  });
+  
+  // Default task(s).
+  grunt.registerTask('default', ['templates','concat','uglify','cssmin']);
+
+};
---
+++ dist/css/screen.min.css
@@ -0,0 +1,3 @@
+/*!
+ Scheduler Build version 0.0.1, 09-05-2013
+*/
